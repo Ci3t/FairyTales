e8f3d43f41c7c49e0fbd6938514028f260dd2b15
mick84 <demenkoff1984@yahoo.com>
2022-12-21 13:48:25 +0200

    mick

--- server/models/tale.js
+++ server/models/tale.js
@@ -1,1 +1,20 @@
-console.log("hello");
+import { set, Schema, model } from "mongoose";
+set("strictQuery", false);
+const taleSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  storyLink: {
+    type: String,
+    required: true,
+  },
+  culture: {
+    type: String,
+    enum: ["arabic", "jewish"],
+    required: true,
+  },
+  data: [String],
+  default: "",
+});
+export const Tale = new model("Tale", taleSchema);
