8857e798a1820c1e52ae88d7eee70f363ae3bd48
Ben663 <87435130+Ben663@users.noreply.github.com>
2022-12-21 16:23:14 +0200

    new

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,42 @@
+// homepage
+// story
+// title story
+
+import{ useState } from "react";
+
+const HomePage = () => {
+    const [state, setState] = useState('');
+    const handleState = (e) => {
+        const value = e.target.value;
+        setState(value);
+    }
+    const hendleSearch = () => {
+        
+    }
+    return (
+			<>
+				<div className='home'>
+					<h1>Welcome</h1>
+					<h3>this is it</h3>
+				</div>
+				<input
+					type='text'
+					value={state}
+					onChange={handleState}
+				/>
+				<button onClick={hendleSearch}>input</button>
+			</>
+		);
+    
+}
+export default HomePage ;
+
+// // 
+// const Homepage () => {
+//     const [query, setQuery] = useState('')
+
+//     const handleQuery = (e) => {
+//         const val = e.target.value;
+//     }
+    
+// }
