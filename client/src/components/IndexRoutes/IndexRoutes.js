ca40246661f638dde1bee2d068ba13d42400741b
Ci3t <ranigfx@gmail.com>
2022-12-21 18:26:07 +0200

    create story

--- client/src/components/IndexRoutes/IndexRoutes.js
+++ client/src/components/IndexRoutes/IndexRoutes.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,Navigate } from "react-router-dom";
+import CreateStory from "../../pages/CreateStory";
 
 function IndexRoutes() {
   return (
     <Routes>
       <Route path="/homepage" element={<p>homepage</p>} />
       <Route path="/stories" element={<p>stories</p>} />
-      <Route path="/new-story" element={<p>new-story</p>} />
+      <Route path="/new-story" element={<CreateStory/>} />
+     
       <Route path="/" element={<Navigate replace to="/homepage" />} />
       <Route path="*" element={<Navigate replace to="/homepage" />} />
     </Routes>
