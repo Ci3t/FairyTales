c83f1e026ac91b3c1f6158fb7a3c9db2aed9779d
Nethanel Bedok <nethanelbedok@gmail.com>
2022-12-21 17:05:22 +0200

    desigin story Itself

---
+++ client/src/components/Stories.jsx
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function Stories(){
+    return (
+        <div  className="Container HomePage">
+            <div className="vector">
+                dfdfdffd
+            </div>
+    </div>
+    )
+    
+}
+export default Stories
