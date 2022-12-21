6ce9f5510c23740ca6dbc09f54e731a9fcdaa75a
mick84 <demenkoff1984@yahoo.com>
2022-12-21 19:23:53 +0200

    talepage

---
+++ client/src/pages/TalePage.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { getSingleStory } from "../components/api/fetch";
+
+export default function TalePage({ taleID, children }) {
+  const [state, setState] = useState({
+    error: null,
+    loading: false,
+    tale: null,
+  });
+  useEffect(() =>
+    setTimeout(async () => {
+      setState((st) => ({ ...st, loading: true }));
+      try {
+        const tale = await getSingleStory(taleID);
+        setState((st) => ({ ...st, tale }));
+      } catch (error) {
+        setState((st) => ({ ...st, error }));
+      } finally {
+        setState((st) => ({ ...st, loading: false }));
+      }
+    }, 0)
+  );
+  return (
+    <div className="page">
+      <h1 className="title">{tale.title}</h1>
+      <div></div>
+    </div>
+  );
+}
