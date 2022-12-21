fc181f797f141b86c2b013a8121f584c8e1142ed
mick84 <demenkoff1984@yahoo.com>
2022-12-21 15:07:02 +0200

    routes

--- server/Routes/routes.js
+++ server/Routes/routes.js
@@ -1,3 +1,13 @@
 import { Router } from "express";
+import { Tale } from "../models/tale.js";
 const router = new Router();
+router.post("/tale", async (req, res) => {
+  try {
+    const { body } = req;
+    const newCreatedTale = await Tale.create(body);
+    res.status(201).json(newCreatedTale);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
 export default router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import "./mongoose/moongose.connect.js";
 import express from "express";
+import cors from "cors";
 import router from "./Routes/routes.js";
 const PORT = process.env.PORT || 5000;
 const server = express();
